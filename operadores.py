fd27d2330c8e5179c804cd76aacad57cad55ba3e
MARIELBLANCO88 <blancohmariel@gmail.com>
2023-12-01 01:25:03 -1200

    commit inicial

---
+++ operadores.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+number = 20
+number2 = 18
+suma = number + number2
+modulo = 20 % 2
+if number < number2:
+    print ("esta condicion es verdadera")
+
+print (suma)
+print(modulo)
+print (number == number2)
---
+++ variables.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+name = "MARIEL"
+age = 20
+number = 13,5
+heigth = 1,88
+is_student = True
+number1 = -15
+absolut = abs (number1)
+name2 = "Movil"
+name3 = name2[0]
+longitud = len(name2)
+minusculas = name2.lower()
+
+print ("hello my name is", name, age)
+print (type (name))
+print (type (age))
+print (type (number))
+print (absolut)
+print ("hello" + " " + "jueves")
+print (name3)
+print (longitud)
+print (minusculas)
